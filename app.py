1630603b1ceb54ccbb4bec427b49808675218fd9
Bruno Martins <gz4256@wayne.edu>
2023-05-05 16:56:23 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        searched_keyword = request.form["search"]
+        df = pd.read_csv("SearchTerms_2022.csv")
+        df = df[df["Search Term"].str.contains(searched_keyword, na=False)]
+        return render_template("index.html", dataframe=df.head(50).to_html(classes="table table-striped", index=False))
+    return render_template("index.html", dataframe=None)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
