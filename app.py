d1f52a68b99b69c846356c0e8103ffc641501178
Bruno Martins <gz4256@wayne.edu>
2023-05-08 15:30:43 -0400

    fixes

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        searched_keyword = request.form["search"]
+        searched_keyword = request.form["search"].lower()
         df = pd.read_csv("SearchTerms_2022.csv")
-        df = df[df["Search Term"].str.contains(searched_keyword, na=False)]
+        df = df[df["Search Term"].str.contains(searched_keyword, na=False, case=False)]
         return render_template("index.html", dataframe=df.head(50).to_html(classes="table table-striped", index=False))
     return render_template("index.html", dataframe=None)
 
