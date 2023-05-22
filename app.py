b8e5f8a073d9c7d863e4bdcc34e3ad606ee875d4
Bruno Martins <gz4256@wayne.edu>
2023-05-22 16:37:15 -0400

    added top 20 results to default when opening the page

--- app.py
+++ app.py
@@ -6,19 +6,22 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     unique_keywords = None
+    df = pd.read_csv('Search_Terms_2023.csv')
+
     if request.method == "POST":
         searched_keyword = request.form["search"].lower()
-        df = pd.read_csv("Search_Terms_2023.csv")
-        df = df[df["Search Term"].str.contains(searched_keyword, na=False, case=False)]
-        unique_words = set()  # Create an empty set to store unique words
-        for term in df["Search Term"].unique():
-            for word in term.split():  # Split each term into words
-                unique_words.add(word.lower())  # Add each word to the set
+        df = df[df['Search Term'].str.contains(searched_keyword, na=False)]
+        unique_words = set()
+        for term in df['Search Term'].unique():
+            for word in term.split():
+                unique_words.add(word.lower())
         unique_keywords = '\n'.join(sorted(list(unique_words)))
         return render_template("index.html", dataframe=df.head(50).to_html(classes="table table-striped", index=False), unique_keywords=unique_keywords)
-    return render_template("index.html", dataframe=None)
+        
+    return render_template("index.html", dataframe=df.head(20).to_html(classes="table table-striped", index=False), unique_keywords=None)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
