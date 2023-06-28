fdd7f64558b575962396b5f048caecef15a31edd
Bruno Martins <gz4256@wayne.edu>
2023-06-28 09:51:20 -0400

    added sqlite

--- app.py
+++ app.py
@@ -1,26 +1,41 @@
-import pandas as pd
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///SearchTerm.db'
+db = SQLAlchemy(app)
+
+engine = create_engine('sqlite:///SearchTerm.db', echo=True)
+
+class SearchTerm(db.Model):
+    __tablename__ = 'SearchTerm'
+    id = db.Column(db.Integer, primary_key=True)
+    SearchTerm = db.Column(db.String(500), nullable=False)
+    SearchFrequencyRank = db.Column(db.Integer, nullable=False)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     unique_keywords = None
-    df = pd.read_csv('Search_Terms_2023.csv')
+    df = pd.read_sql_table('SearchTerm', con=engine)
 
     if request.method == "POST":
         searched_keyword = request.form["search"].lower()
-        df = df[df['Search Term'].str.contains(searched_keyword, na=False)]
+        df = df[df['SearchTerm'].str.contains(searched_keyword, na=False)]
         unique_words = set()
-        for term in df['Search Term'].unique():
-            for word in term.split():
+        for SearchTerm in df['SearchTerm'].unique():
+            for word in SearchTerm.split():
                 unique_words.add(word.lower())
         unique_keywords = '\n'.join(sorted(list(unique_words)))
-        return render_template("index.html", dataframe=df.head(50).to_html(classes="table table-striped", index=False), unique_keywords=unique_keywords)
-        
+        df = df.drop('id', axis=1)
+        return render_template("index.html", dataframe=df.head(300).to_html(classes="table table-striped", index=False), unique_keywords=unique_keywords)
+    
+    df = df.drop('id', axis=1)
     return render_template("index.html", dataframe=df.head(20).to_html(classes="table table-striped", index=False), unique_keywords=None)
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
 
 
